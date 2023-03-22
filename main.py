d8d6cf4141dab8bf71ca4ba51f952ece57be1035
Hector DelaRosa <hdelaros@gmail.com>
2023-03-21 21:53:18 -0400

    Added Twilio functionality and news article formatting logic

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 from stocks import stock_data
 from news import news_articles
-import requests
 from twilio.rest import Client
+import os
+
+STOCK_SYMBOL = 'TSLA'
 
 
 def calculate_percentage(yesterday, day_before):
-    diff = abs(float(yesterday) - float(day_before))
+    diff = float(yesterday) - float(day_before)
     percent = round(diff / float(day_before) * 100)
     return percent
 
 
 stocks_data_list = [value for (key, value) in stock_data.items()]
 
-print(stocks_data_list)
+print(news_articles)
 yesterday_closing_price = stocks_data_list[0]['4. close']
 day_before_yesterday_closing_price = stocks_data_list[1]['4. close']
 
 percentage = calculate_percentage(yesterday_closing_price, day_before_yesterday_closing_price)
+up_or_down = None
+if percentage > 0:
+    up_or_down = '🔺'
+else:
+    up_or_down = '🔻'
 
-if percentage >= 2:
-    print('Get News')
+if percentage > 4:
+    top_three_articles = news_articles[:3]
+
+    for article in top_three_articles:
+        msg = f'{STOCK_SYMBOL}: {up_or_down}{abs(percentage)}%\n' \
+              f'Headline: {article["title"]}\n' \
+              f'Brief: {article["content"]}'
+        print(msg)
+        account_sid = os.environ['TWILIO_ACCOUNT_SID']
+        auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            body=msg,
+            from_=os.environ['FROM_NUM'],
+            to=os.environ['TO_NUM'],
+        )
+        print(message.sid)
